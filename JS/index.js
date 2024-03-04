457d2c071426fee46dd5a9d8f6ef1c64bfa70113
lego0821 <155293117+lego0821@users.noreply.github.com>
2024-03-04 22:24:55 +0900

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -19,7 +19,7 @@ window.addEventListener('load',function(){
   const notiIndex = Cookies.get('notification');
   if(Number(notiIndex) < 2 || notiIndex === undefined || notiIndex === null){
     document.querySelectorAll('li a')[2].style.color = '#fcb6aa';
-    document.querySelectorAll('li a')[2].class = 'navs dot';
+    document.querySelectorAll('li a')[2].className = 'navs dot';
   }
   if(location.href === 'https://lego0821.github.io/lego0821/pages/notification/'){
     Cookies.set('notification',2,{expires: 30});
